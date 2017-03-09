d3728776530e45c8edef0f4d86911cf32bfb8795
Holmgren <kristofer.holmgren@sogeti.com>
2017-03-09 09:11:24 +0100

    Added powershell script that starts three processes: webpack -w, game-server and content-server.

---
+++ spyvsspy/spydev.ps1
@@ -0,0 +1,3 @@
+start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command npm run spyvsspywebpack'
+start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command npm run spyvsspygameserver'
+start-process powershell.exe -argument '-nologo -noprofile -executionpolicy bypass -command npm run spyvsspycontentserver'
