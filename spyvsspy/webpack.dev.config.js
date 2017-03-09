da8ecda68ad392f3dc03328af90a7b86df246816
Holmgren <kristofer.holmgren@sogeti.com>
2017-03-09 14:02:33 +0100

    Added two config files. Changes clients server-url depending on environment when webpack builds distribution

---
+++ spyvsspy/webpack.dev.config.js
@@ -0,0 +1,40 @@
+
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'API_URL': "\"http://localhost:3000/\""
+            }
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            }
+        ]
+    }
+};
