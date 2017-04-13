427566d56359bb0f59e5a462b88c478eee63b959
Holmgren <kristofer.holmgren@sogeti.com>
2017-04-13 14:09:34 +0200

    Added server for playground

--- playground/httpsServer.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var app = express();
-
-http.createServer(app).listen(80);
-https.createServer(options, app).listen(443);
