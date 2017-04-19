5b9b31036630820e6d77eb87ce6d5a41856b5b64
Mikael Vesavuori <mikael.vesavuori@sogeti.com>
2017-04-19 13:43:04 +0200

    Sync project

--- playground/tictactoe/src/client/client.ts
+++ playground/tictactoe/src/client/client.ts
@@ -3,132 +3,135 @@ import { ListGames } from '../shared/listGames';
 import { GameStatus } from '../server/gameSession';
 
 export class Client {
-    currentGame: GameState = null;
-    socket: any;
-    readonly boardElement: HTMLElement = document.getElementById('board');
+	//currentGame: GameState = null; // Production
+	currentGame: any = null; // Debug
+	socket: any;
+	readonly boardElement: HTMLElement = document.getElementById('board');
 
-    constructor(socket: any) {
-        this.socket = socket;
+	constructor(socket: any) {
+		this.socket = socket;
 
-        this.socket.on('game-update', this.gameUpdate.bind(this));
-        this.socket.on('game-created', this.gameCreated.bind(this));
-        this.socket.on('game-started', this.startGame.bind(this));
-        this.socket.on('game-ended', this.createGameOver.bind(this));
-        this.socket.on('player-disconnected', this.gameCancelled.bind(this));
+		this.socket.on('game-update', this.gameUpdate.bind(this));
+		this.socket.on('game-created', this.gameCreated.bind(this));
+		this.socket.on('game-started', this.startGame.bind(this));
+		this.socket.on('game-ended', this.createGameOver.bind(this));
+		this.socket.on('player-disconnected', this.gameCancelled.bind(this));
 
-        this.createMenu();
-    }
+		this.createMenu();
+	}
 
-    public createBoard(): void {
+	public createBoard(): void {
 
-        let count = 1;
-        let column = 0;
-        let row = 0;
+		let count = 1;
+		let column = 0;
+		let row = 0;
 
-        for (const prop in this.currentGame.board.boxes) {
+		for (const prop in this.currentGame.board.boxes) {
 
-            let zpos = 0;
+			let zpos = 0;
 
-            if (count === 1 || count === 4 || count === 7) {
-                zpos = 0;
-            } else if (count === 2 || count === 5 || count === 8) {
-                zpos = 0.4;
-            } else if (count === 3 || count === 6 || count === 9) {
-                zpos = 0.8;
-            }
+			if (count === 1 || count === 4 || count === 7) {
+				zpos = 0;
+			} else if (count === 2 || count === 5 || count === 8) {
+				zpos = 0.4;
+			} else if (count === 3 || count === 6 || count === 9) {
+				zpos = 0.8;
+			}
 
-            const html = '<a-obj-model id="' + prop + '" src="#board-obj" mtl="#board-mtl" position="' + row + ' 0 ' + zpos + '" scale="0.2 1 0.2"></a-obj-model>';
+			const html = '<a-obj-model id="' + prop + '" src="#board-obj" mtl="#board-mtl" position="' + row + ' 0 ' + zpos + '" scale="0.2 1 0.2"></a-obj-model>';
 
-            const newElement = document.createElement('a-entity');
-            newElement.setAttribute('id', prop);
-            newElement.setAttribute('cursor-listener', '');
-            newElement.innerHTML = html;
+			this.boardElement.setAttribute("position", "-0.5 0 -0.5");
 
-            this.boardElement.appendChild(newElement);
-            count++;
-            if (count > 3 && count < 6) {
-                row = 0.4;
-            } else if (count > 6) {
-                row = 0.8;
-            }
-        };
-    }
+			const newElement = document.createElement('a-entity');
+			newElement.setAttribute('id', prop);
+			newElement.setAttribute('cursor-listener', '');
+			newElement.innerHTML = html;
 
-    private createMenu(): void {
-        // Create plane elements for create game and join game
+			this.boardElement.appendChild(newElement);
+			count++;
+			if (count > 3 && count < 6) {
+				row = 0.4;
+			} else if (count > 6) {
+				row = 0.8;
+			}
+		};
+	}
 
-        this.cleanBoard();
+	private createMenu(): void {
+		// Create plane elements for create game and join game
 
-        const menuHtml =
-            `<a-box position='0 0 0' material='opacity: 0.5;'>
-            <a-text value="New game" id="newgame" sides="both" rotation="-90 0 0" menu-select position="-0.5 0.5 0.3"></a-text>
-            <a-text value="Join game" id="joingame" sides="both" rotation="-90 0 0" menu-select position="-0.5 0.5 -0.3"></a-text>
-        </a-box>`;
+		this.cleanBoard();
 
-        this.boardElement.innerHTML = menuHtml;
-    }
+		const menuHtml =
+			`<a-box position='0 0 0' material='opacity: 0.5;'>
+			<a-text value="New game" id="newgame" sides="both" rotation="-90 0 0" menu-select position="-0.5 0.5 0.3"></a-text>
+			<a-text value="Join game" id="joingame" sides="both" rotation="-90 0 0" menu-select position="-0.5 0.5 -0.3"></a-text>
+		</a-box>`;
 
-    private createLobby(): void {
-        this.cleanBoard();
+		this.boardElement.innerHTML = menuHtml;
+	}
 
-        const lobbyHtml =
-            `<a-box position='0 0 0' material='opacity: 0.5;'>
-            <a-text value="Waiting.." id="newgame" sides="both" rotation="-90 0 0" menu-select position="-0.5 0.5 -0.3"></a-text>
-        </a-box>`;
+	private createLobby(): void {
+		this.cleanBoard();
 
-        this.boardElement.innerHTML = lobbyHtml;
-    }
+		const lobbyHtml =
+			`<a-box position='0 0 0' material='opacity: 0.5;'>
+			<a-text value="Waiting.." id="newgame" sides="both" rotation="-90 0 0" menu-select position="-0.5 0.5 -0.3"></a-text>
+		</a-box>`;
 
-    private createUi(): void {
-        // Create element that shows wich player has the current turn
-    }
+		this.boardElement.innerHTML = lobbyHtml;
+	}
 
-    private cleanBoard(): void {
-        // Remove board entity from dom
-        this.boardElement.innerHTML = '';
-    }
+	private createUi(): void {
+		// Create element that shows wich player has the current turn
+	}
 
-    private createGameOver(winningPlayer: string): void {
-        // Create element to show winning players name
-    }
+	private cleanBoard(): void {
+		// Remove board entity from dom
+		this.boardElement.innerHTML = '';
+	}
 
-    gameCreated(gameState: GameState): void {
-        // Show lobby
-        this.currentGame = gameState;
-        this.createLobby();
-    }
+	private createGameOver(winningPlayer: string): void {
+		// Create element to show winning players name
+	}
 
-    // Alert player that game was cancelled, cleanup and create display menu
-    gameCancelled(reason: string): void {
-        this.createGameOver(this.currentGame.board.winner);
+	gameCreated(gameState: GameState): void {
+		// Show lobby
+		this.currentGame = gameState;
+		this.createLobby();
+	}
 
-        setTimeout(() => {
-            this.createMenu();
-            this.currentGame = null;
-        });
-    }
+	// Alert player that game was cancelled, cleanup and create display menu
+	gameCancelled(reason: string): void {
+		this.createGameOver(this.currentGame.board.winner);
 
-    startGame(gameState: GameState): void {
-        this.cleanBoard();
-        // Create board
-        this.createBoard();
-    }
+		setTimeout(() => {
+			this.createMenu();
+			this.currentGame = null;
+		});
+	}
 
-    endGame(): void {
-        this.createGameOver();
+	startGame(gameState: GameState): void {
+		this.cleanBoard();
+		// Create board
+		this.createBoard();
+	}
 
-        setTimeout(() => {
-            this.createMenu();
-        }, 8000);
-    }
+	endGame(): void {
+		this.createGameOver();
 
-    gameUpdate(gameState: GameState) {
-        // examine changes in state and update accordingly
-        // an update can be one of two things: 
-        // a player has placed a marker
-        // or the game is over
-        // if a player has moved this should be represented by an animation of placing the marker
-        // if a player has won, the screen should fade to black and the winning players name showed
-        // after x seconds, set currentGame to null and return to lobby
-    }
+		setTimeout(() => {
+			this.createMenu();
+		}, 8000);
+	}
+
+	gameUpdate(gameState: GameState) {
+		// examine changes in state and update accordingly
+		// an update can be one of two things:
+		// a player has placed a marker
+		// or the game is over
+		// if a player has moved this should be represented by an animation of placing the marker
+		// if a player has won, the screen should fade to black and the winning players name showed
+		// after x seconds, set currentGame to null and return to lobby
+	}
 }
--- playground/tictactoe/src/client/main.ts
+++ playground/tictactoe/src/client/main.ts
@@ -6,35 +6,36 @@ declare var AFRAME: any;
 const url: string = process.env.API_URL;
 
 window.addEventListener("load", function () {
-    const socket = io.connect('https://localhost:3000', { reconnection: false });
+	const socket = io.connect('https://localhost:3000', { reconnection: false });
 
-    AFRAME.registerComponent('menu-select', {
-        init: function () {
-            this.el.addEventListener('click', function (evt: any) {
-                // check element id, send message with name of element id 
-                // to create or join game
-                if (evt.currentTarget.id === 'newgame') {
-                    console.log('creating game');
-                    socket.emit('create-game');
-                } else {
-                    // Not sure if this is the best place to handle the join channel logic
-                    console.log('joining game');
-                    socket.emit('join-game');
-                }
-            });
-        }
-    });
+	AFRAME.registerComponent('menu-select', {
+		init: function () {
+			this.el.addEventListener('click', function (evt: any) {
+				// check element id, send message with name of element id
+				// to create or join game
+				if (evt.currentTarget.id === 'newgame') {
+					console.log('creating game');
+					socket.emit('create-game');
+				} else {
+					// Not sure if this is the best place to handle the join channel logic
+					console.log('joining game');
+					socket.emit('join-game');
+				}
+			});
+		}
+	});
 
-    AFRAME.registerComponent('cursor-listener', {
-        init: function () {
-            this.el.addEventListener('click', function (evt: any) {
-                // check element id, send message with name "place-marker" 
-                if (client.currentGame.playerCurrentTurn === socket.id) {
-                    socket.emit('place-marker', evt.currentTarget.id);
-                }
-            });
-        }
-    });
+	AFRAME.registerComponent('cursor-listener', {
+		init: function () {
+			this.el.addEventListener('click', function (evt: any) {
+				// check element id, send message with name "place-marker"
+				if (client.currentGame.playerCurrentTurn === socket.id) {
+					socket.emit('place-marker', evt.currentTarget.id);
+				}
+			});
+		}
+	});
 
-    const client = new Client(socket);
+	const client = new Client(socket);
+	client.createBoard();
 });
